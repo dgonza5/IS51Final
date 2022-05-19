5647ceb8342d4335d09a1d946ef542ae7086a950
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-05-19 16:11:54 -0700

    structured-english

---
+++ final.py
@@ -0,0 +1,16 @@
+"""
+The program is trying to determine from the students grades resulting from the final exam.
+First, display the number of grades.
+Second, display the average grade.
+Third, display the percentage of grades that are above the average grade. 
+
+Number of grades: 24
+Average grade: 83.25
+Percentage of grades above average: 54.17
+
+There will be two functions to calculate the grades. 
+
+Fuction-1 will kickstart the application, it will extract the information from the Final.txt
+file and then calculate the average grade and then close the file. 
+Function-2 will calculate the percentage of grades that are above the average grade. 
+"""
