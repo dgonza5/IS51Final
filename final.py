7d491e6bb230ef43fcf3cc981f2277d329d1ab4c
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-05-19 17:06:49 -0700

    exam-end

--- final.py
+++ final.py
@@ -27,3 +27,27 @@ above_average():
     read list and pull info
     close
 """
+def main():
+    infile = open("Final.txt", "r")
+    grades = []
+    for line in infile:
+        grades.append(int(line.strip()))
+    print("Number of grades: ", len(grades))
+
+    average = 0
+    for grade in grades:
+        average += grade
+    average /= len(grades)
+    print("Average grade: ", average)
+    print("Percentage of grades above average: {0:.2f}%".format(calculate_percent_above_average(grades, average)))
+    infile.close()
+
+def calculate_percent_above_average(grades, average):
+    count = 0
+    for grade in grades:
+        if grade > average:
+            count += 1
+    return (count * 100) / len(grades)
+
+
+main()
